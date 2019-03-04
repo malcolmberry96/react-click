dee10a8c8b88755129f084f231e603cc59e34d8c
malcolmberry96 <malcolm.berry65@gmail.com>
2019-03-04 17:30:00 -0600

    instructions format update

--- src/components/Score/index.js
+++ src/components/Score/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import "./style.css";
 
 function Score(props) {
-    return <p className="score">
-    Try to remember all twelve sneakers! Click the same sneaker twice and you lose. Try to remember all twelve!
-   <br> Score: {props.children} </br>
-   </p>;
-    
+    return(
+    <div>
+        <p className="score"> Score: {props.children}  </p>
+         <br> Try to remember all twelve sneakers! Click the same sneaker twice and you lose. Try to remember all twelve!</br>
+   </div>
+    );
 }
 
 export default Score;
